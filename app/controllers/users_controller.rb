b629c036454c53ac0b28bc7fa9229b76a2c4f9d1
sairuzu <l4_1960st@yahoo.co.jp>
2023-10-28 00:17:16 +0900

    fourth commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
 
-  def  show
+  def show
     @user = User.find(params[:id])
-    @prototype = Prototype.find(params[:id]) 
+    @user.prototypes
+    
+    @prototype = Prototype.find(params[:id])
+    
   end
 
 
