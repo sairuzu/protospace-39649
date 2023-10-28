bf50fec5f0c6e37a72138e11c16f0e52d2daa659
sairuzu <l4_1960st@yahoo.co.jp>
2023-10-28 15:04:49 +0900

    six commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,13 +2,11 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
-
 
   def index
     @prototypes = Prototype.includes(:user)
   end
-  
+
   def new
     @prototype = Prototype.new
   end
@@ -23,29 +21,22 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    
     @comment = Comment.new
     @comments = @prototype.comments
-    
-    
-    
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to prototype_path
+      redirect_to prototype_path(@prototype)
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     if @prototype.destroy
       redirect_to root_path
     else
@@ -54,10 +45,11 @@ class PrototypesController < ApplicationController
   end
 
   private
+
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-  
+
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
@@ -65,13 +57,4 @@ class PrototypesController < ApplicationController
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
-  
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-
-  end
-
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
-    @user.prototypes
-    
-    @prototype = Prototype.find(params[:id])
-    
   end
-
-
 end
